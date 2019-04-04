91e8107ea6ead01021751d4dccbbdd76c42f6580
Mario Schlack <hurikhan@protonmail.com>
2019-04-04 16:45:09 +0800

    Added cvar client_maximized.

--- gdprogs/client/client.gd
+++ gdprogs/client/client.gd
@@ -9,6 +9,14 @@ func _ready():
 		min_value = 0,
 		max_value = 1
 	})
+	console.register_cvar("client_maximized", {
+		node = self,
+		description = "Switches Window Maximized on/off",
+		type = "int",
+		default_value = 0,
+		min_value = 0,
+		max_value = 1
+	})
 
 #      ___ ___  _ ____   ____ _ _ __   
 #     / __/ _ \| '_ \ \ / / _` | '__|  
@@ -21,3 +29,10 @@ func _convar_client_fullscreen(value):
 		OS.set_window_fullscreen(true)
 	else:
 		OS.set_window_fullscreen(false)
+
+
+func _convar_client_maximized(value):
+	if value == 1:
+		OS.set_window_maximized(true)
+	else:
+		OS.set_window_maximized(false)
